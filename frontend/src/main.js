07a226585146db8d2a66e055ac09c8251b984cd8
Wong Qi Heng <wongqiheng06@gmail.com>
2026-02-21 12:11:27 +0800

    added a few buttons and their logic

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,2 +1,33 @@
-import { setupCounter } from './counter.js'
-setupCounter(document.querySelector('#counter'))
+const sx_slider = document.getElementById("sensitivity-x-slider")
+const sy_slider = document.getElementById("sensitivity-y-slider")
+
+const sx_label = document.getElementById("sensitivity-x-val")
+const sy_label = document.getElementById("sensitivity-y-val")
+
+// initialize value
+sx_label.innerText = sx_slider.value
+sy_label.innerText = sy_slider.value
+
+// change value as user changes input
+sx_slider.addEventListener("input", function() {
+  sx_label.innerText = sx_slider.value
+});
+
+sy_slider.addEventListener("input", function() {
+  sy_label.innerText = sy_slider.value
+});
+
+// show the active page and hides the inactive page
+function showPage(pageID) {
+  const pages = document.querySelectorAll('.page');
+  pages.forEach(page => page.classList.remove('active'));
+
+  document.getElementById(pageID).classList.add('active');
+}
+
+document.getElementById("connectBtn").addEventListener(
+  "click", () => showPage("connectPage")
+);
+document.getElementById("settingBtn").addEventListener(
+  "click", () => showPage("settingPage")
+);
